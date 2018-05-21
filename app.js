c23648406e284d9e74452701c6d1a45a0b18f3ad
satya <satyadev.kadaganchi@gmail.com>
2018-05-21 13:28:18 +0200

    express webserver setup

--- app.js
+++ app.js
@@ -1,40 +1,45 @@
 var http = require('http');
 var fs = require('fs');
 var colors = require('colors');
-require('./lib/connection');
+//require('./lib/connection');
 var m = require('./math-module/mmm');
-var mongoose = require('mongoose');
+//var mongoose = require('mongoose');
 //var db = mongoose.connection;
+var express = require('express');
+var app =express();
+
+//Route one
+app.get('/teams/:teamName/employees/:employeeId', function(req, res, next){
+    console.log('teamName =' + req.params.teamName);
+    console.log('employeeId =' + req.params.employeeId);
+    res.send("path one");
+})
+
+//Route two
+app.get('/teams/:teamName/employees', function(req, res, next) {
+    console.log('Setting content type');
+    res.set('Content-Type', 'application/json');
+    res.locals.data = 100;
+    next();
+}, function (req, res, next) {
+    console.log('teamName =' + req.params.teamName);
+    console.log(res.locals.data);
+    res.send('path two');
+});
+
+//Route three
+app.get(/^\groups\/(\w+)\/(\d+)$/, function(req, res, next) {
+    console.log('groupname =' + req.params[0]);
+    console.log('groupId = ' + req.params[1]);
+    res.send('path three');
+});
+
+var server = app.listen(1337, function() {
+    console.log('Server startedon port 1337');
+});
 
 
 
-http.createServer(function (req,res){
-    if(req.url === '/favicon.ico'){
-        return res.end();
-    }
-    console.log('Incoming request to ' +req.url);
-    
-    var i = 2;
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    
-    setTimeout(function(){
-        fs.readFile(__filename,{
-            encoding: 'utf8'
-        }, function (error, contents){
-            if (error) {
-                console.error(error);
-                return res.end();
-            }
-            console.log('sending response for' + req.url);
-            res.end(contents);
-        });
-    },5000);
-    
-    while(i--){
-        console.log('Loop value:' +i + '\r');
-    }
-    
-    
-}).listen(1337, '127.0.0.1');
+
 
 console.log('Server running at http://127.0.0.1:1337/');
